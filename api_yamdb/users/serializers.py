27275f5f272344c8fa01b7866620a0548d2fffcd
nublin <polina.ivoylova1@gmail.com>
2025-04-18 12:01:57 +0400

    Setting up a basic user serializer and user model view set

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,10 +1,28 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator
 
 User = get_user_model()
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class BaseUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    username = serializers.CharField(
+        validators=[
+            RegexValidator(
+                regex=r'^[\w.@+-]+\Z',
+                message='Username может содержать только буквы, цифры и @/./+/-/_'
+            )
+        ]
+    )
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'bio')
+        fields = ('username', 'email')
+
+    def validate_username(self, value):
+        if value.lower() == 'me':
+            raise serializers.ValidationError(
+                'Использовать имя "me" в качестве username запрещено.'
+            )
+        return value
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,12 +1,14 @@
-from rest_framework import viewsets
 from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
-from .serializers import CustomUserSerializer
+from .serializers import BaseUserSerializer
 
 User = get_user_model()
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
-    serializer_class = CustomUserSerializer
+    serializer_class = BaseUserSerializer
+    permission_classes = (IsAdminUser,)
     lookup_field = 'username'
