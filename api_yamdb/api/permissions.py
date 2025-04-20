e80ed09b80bb32247bd16166f1ad30c9aa82c34c
Hayk Gevorkyan <gevorkyan.ayk1904@gmail.com>
2025-04-20 11:52:16 +0300

    Permissions Update

---
+++ api_yamdb/api/permissions.py
@@ -0,0 +1,24 @@
+from rest_framework import permissions
+
+
+class IsAuthorOrModeratorOrAdminOrReadOnly(permissions.BasePermission):
+    """
+    Читать могут все.
+    Писать — аутентифицированные.
+    Редактировать и удалять — автор или модератор или админ.
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return (
+            obj.author == request.user
+            or getattr(request.user, 'role', None) in ('moderator', 'admin')
+            or request.user.is_staff
+        )
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,16 +1,19 @@
 from rest_framework import permissions, status, viewsets
 from rest_framework.exceptions import PermissionDenied
+from django.shortcuts import get_object_or_404
+
 
 from api.serializers import CommentSerializer, ReviewSerializer
+from .permissions import IsAuthorOrModeratorOrAdminOrReadOnly
 from reviews.models import Comment, Review
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthorOrModeratorOrAdminOrReadOnly,)
 
     def get_queryset(self):
-        return Comment.objects.filter(review_id=self.kwargs['review_id'])
+        return Comment.objects.filter(review_id=self.kwargs['title_id'])
 
     def perform_create(self, serializer):
         serializer.save(
@@ -19,38 +22,26 @@ class CommentViewSet(viewsets.ModelViewSet):
         )
 
     def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого комментария запрещено!')
         serializer.save()
 
     def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Удаление чужого комментария запрещено!')
         instance.delete()
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthorOrModeratorOrAdminOrReadOnly,)
 
     def get_queryset(self):
         title_id = self.kwargs.get('title_id')
         return Review.objects.filter(title_id=title_id)
 
     def perform_create(self, serializer):
-        title_id = self.kwargs.get('title_id')
-        try:
-            title = Title.objects.get(id=title_id)
-        except Title.DoesNotExist:
-            raise NotFound('Произведение не найдено')
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
         serializer.save(author=self.request.user, title=title)
 
     def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого отзыва запрещено!')
         serializer.save()
 
     def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Удаление чужого отзыва запрещено!')
         instance.delete()
